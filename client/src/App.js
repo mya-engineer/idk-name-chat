466c3bdda92e333f217500ff69a7e38ab27fa190
mya <yatsenkomy@yandex.ru>
2021-10-18 16:40:41 +0300

    react client initialized

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from 'rsuite'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <Button appearance='primary'> Hello world </Button>
+    </div>
+  )
+}
+
+export default App
